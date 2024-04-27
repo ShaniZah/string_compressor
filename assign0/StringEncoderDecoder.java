a04e2b84e1f1b13aa6554bb88501a0bacc0c0270
shani zahavi <shanizahavi21@gmail.com>
2024-04-27 20:21:45 +0300

    finished compression and decompressiom

--- assign0/StringEncoderDecoder.java
+++ assign0/StringEncoderDecoder.java
@@ -16,49 +16,91 @@ import base.Compressor;
 
 public class StringEncoderDecoder implements Compressor {
 
+	HashMap<String, Integer> words_from_input;
+	HashMap<String, Integer> common_words;
+
 	public StringEncoderDecoder() {
-		// TODO Auto-generated constructor stub
+		words_from_input = new HashMap<>(); // string = words, int = appearances
+		common_words = new HashMap<>(); // string = words, int = appearances
 	}
 
 	@Override
 	public void Compress(String[] input_names, String[] output_names) {
-		char specialChar = 222;
-		HashMap<String, Integer> hash = new HashMap<>(); // string = words, int = appearances
-		String[] words = input_names[0].split("[\\s,.]+"); // regular expression
-		for (int i = 0; i < words.length; i++) {
-			if (words[i].length() >= 3) {
-				// if the word is not there, add it
-				if (!hash.containsKey(words[i])) {
-					hash.put(words[i], 1);
-				} else {
-					// the word exists, increment appearance
-					hash.put(words[i], hash.get(words[i]) + 1);
-				}
+		String[] words = input_names[0].split("[\\s,.!]+"); // regular expression
+		StringBuilder output = new StringBuilder();
+		char specialChar_1 = 222;
+		char specialChar_2 = 223;
+		
+		countWordFrequency(words);
+		createIndexMapping();
+
+		for (String word : words) {
+			if (common_words.containsKey(word)) {
+				output.append((char) specialChar_1);
+				output.append(common_words.get(word));
+			} else {
+				output.append((char) specialChar_2);
+				output.append(word);
 			}
 		}
-		// now we sort the map to have the words with the most appearances.
+		
+		
+		output_names[0] = output.toString();
+		System.out.println(output.toString());
+	}
+
+	private void createIndexMapping() {
+		// sort the map to have the words with the most appearances.
 		// convert map to list
-		ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(hash.entrySet());
+		ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(words_from_input.entrySet());
 		list.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
-		HashMap<String, Integer> sortedmap = new HashMap<>(); // string = words, int = appearances
 
 		// insert to map with encoding
 		for (int i = 0; i < list.size(); i++) {
 			if (i < 200) {
-				sortedmap.put(list.get(i).getKey(), i+1);
+				common_words.put(list.get(i).getKey(), i + 1);
 			}
 			else {
-				sortedmap.put(list.get(i).getKey(), list.get(i).getValue());
+				break;
 			}
 		}
-		
+	}
 
+	private void countWordFrequency(String[] words) {
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].length() >= 3) {
+				// if the word is not there, add it
+				if (!words_from_input.containsKey(words[i])) {
+					words_from_input.put(words[i], 1);
+				} else {
+					// the word exists, increment appearance
+					words_from_input.put(words[i], words_from_input.get(words[i]) + 1);
+				}
+			}
+		}
 	}
 
 	@Override
 	public void Decompress(String[] input_names, String[] output_names) {
-		// TODO Auto-generated method stub
+		String[] codedWords = output_names[0].split("[Þß]+"); // regular expression
 
+		StringBuilder output = new StringBuilder();
+
+		ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(common_words.entrySet());
+		list.sort(Map.Entry.comparingByValue());
+
+		for (int i = 1; i < codedWords.length; i++) {
+			String word = codedWords[i];
+			try { // try parsing the coded word to int, if failed -> not in most common 200
+				int value = Integer.parseInt(word);
+				output.append(list.get(value-1).getKey());
+			} catch (NumberFormatException e) {
+				output.append(word);
+			}
+			output.append(" ");
+		}
+		
+		System.out.println(output.toString());
 	}
 
 	@Override
--- assign0/main.java
+++ assign0/main.java
@@ -6,8 +6,13 @@ public class main {
 		// TODO Auto-generated method stub
 		StringEncoderDecoder str = new StringEncoderDecoder();
 		String[] s = new String[1];
-		s[0] = "I like. strings, pffft, be strings strings like like like";
-		str.Compress(s, args);
+		String[] t = new String[1];
+
+		s[0] = "I like strings, pffft, be strings can't";
+		System.out.println(s[0]);
+		str.Compress(s, t);
+		
+		str.Decompress(s, t);
 	}
 
 }
